2a7619fd8c4bac4dfb08d08dde7eccd06b747a76
Belachkin <belkin.job123@gmail.com>
2025-05-19 23:54:12 +0300

    add sass header and card

--- react_sneakers/src/App.jsx
+++ react_sneakers/src/App.jsx
@@ -1,33 +1,100 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import 'macro-css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='wrapper clear'>
+        <header className='d-flex justify-between align-center p-40'>
+          
+
+              <div className='headerLeft d-flex align-center'>
+                <img src="/logo.png" alt="" />
+                <div className='headerInfo'>
+                  <h3 className='text-uppercase'>React Sneakers</h3>
+                  <p className='opacity-5'>Магазин реактивных кроссовок</p>
+                </div>
+              </div>
+
+            <div className='headerRight mr-30 d-flex'>
+
+              <ul className='d-flex'>
+                <li className='mr-30'>
+                  <img src="/shopping-bag.png" alt="" />
+                  <span>1205 руб.</span>
+                </li>
+                <li>
+                  <img src="/user.png" alt="" />
+                </li>
+              </ul>
+
+            </div>
+          
+          
+          
+        </header>
+
+        <div className="content p-40">
+          <h1 className='mb-40'> Все кроссовки</h1>
+
+          <div className="sneakers d-flex">
+
+            <div className="card">
+              <img src='/sneakers/s1.png' height={100} width={170} />
+              <h5>Реактивные кроссовки: Nike Her Znaet 1</h5>
+              <div className='cardBottom d-flex justify-between align-center'>
+
+                <div className='d-flex flex-column'>
+                  <span>Цена:</span>
+                  <b>12 999 руб.</b>
+                </div>
+
+                <button className='addButton'>
+                  <img src="/plus.png" height={20} width={20} />
+                </button>
+              </div>
+            </div>
+
+            <div className="card">
+              <img src='/sneakers/s2.png' height={100} width={170} />
+              <h5>Реактивные кроссовки: Nike Her Znaet 2</h5>
+              <div className='cardBottom d-flex justify-between align-center'>
+
+                <div className='d-flex flex-column'>
+                  <span>Цена:</span>
+                  <b>12 999 руб.</b>
+                </div>
+
+                <button className='addButton'>
+                  <img src="/plus.png" height={20} width={20} />
+                </button>
+              </div>
+            </div>
+
+            <div className="card">
+              <img src='/sneakers/s3.png' height={100} width={170} />
+              <h5>Реактивные кроссовки: Air Jordan 115 </h5>
+              <div className='cardBottom d-flex justify-between align-center'>
+
+                <div className='d-flex flex-column'>
+                  <span>Цена:</span>
+                  <b>12 999 руб.</b>
+                </div>
+
+                <button className='addButton'>
+                  <img src="/plus.png" height={20} width={20} />
+                </button>
+              </div>
+            </div>
+
+          </div>
+
+          
+
+        </div>
+
+      </div>    
     </>
   )
 }
