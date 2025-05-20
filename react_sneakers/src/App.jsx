2334a503e49ab5776d83aa431b22c7300daccddb
Belachkin <belkin.job123@gmail.com>
2025-05-20 13:03:31 +0300

    add shoppingCart and CartItems in page

--- react_sneakers/src/App.jsx
+++ react_sneakers/src/App.jsx
@@ -6,6 +6,27 @@ function App() {
   return (
     <>
       <div className='wrapper clear'>
+
+        <div className="overlay">
+
+          <div className="drawer">
+              <h2 className='mb-20'>Корзина</h2>
+
+              <div className="cartItem d-flex align-center">
+                <img src="/sneakers/s1.png" alt="" height={60} width={102} className='mr-20' />
+
+                <div>
+                  <p className='mb-5'>Реактивные кроссовки: Nike Her Znaet 1</p>
+                  <b>12 999 руб.</b>
+                </div>
+                <button className='mr-5'>
+                  <img src="/icons/RemoveButton.svg" alt="" height={50} width={50} />
+                </button>
+              </div>
+          </div>
+
+        </div>
+
         <header className='d-flex justify-between align-center p-40'>
           
 
@@ -35,12 +56,26 @@ function App() {
           
         </header>
 
-        <div className="content p-40">
-          <h1 className='mb-40'> Все кроссовки</h1>
+        <div className="content p-40 ">
+          <div className='mb-40 d-flex justify-between'>
+            <h1 className> Все кроссовки</h1>
+            
+            <div className='searchBlock d-flex align-center'>
+              <img src="/icons/search.png" alt="Search" height={20} width={20} />
+              <input placeholder='Найти тапки...' /> 
+            </div>
+            
+            
+          </div>
+          
 
           <div className="sneakers d-flex">
 
             <div className="card">
+              <button className='unlikedButton'>
+                <img src="/icons/UnlikeButton.svg" alt="Unliked" height={25} width={25} />
+              </button>
+              
               <img src='/sneakers/s1.png' height={100} width={170} />
               <h5>Реактивные кроссовки: Nike Her Znaet 1</h5>
               <div className='cardBottom d-flex justify-between align-center'>
@@ -51,39 +86,7 @@ function App() {
                 </div>
 
                 <button className='addButton'>
-                  <img src="/plus.png" height={11} width={11} />
-                </button>
-              </div>
-            </div>
-
-            <div className="card">
-              <img src='/sneakers/s2.png' height={100} width={170} />
-              <h5>Реактивные кроссовки: Nike Her Znaet 2</h5>
-              <div className='cardBottom d-flex justify-between align-center'>
-
-                <div className='d-flex flex-column'>
-                  <span>Цена:</span>
-                  <b>12 999 руб.</b>
-                </div>
-
-                <button className='addButton'>
-                  <img src="/plus.png" height={11} width={11} />
-                </button>
-              </div>
-            </div>
-
-            <div className="card">
-              <img src='/sneakers/s3.png' height={100} width={170} />
-              <h5>Реактивные кроссовки: Air Jordan 115 </h5>
-              <div className='cardBottom d-flex justify-between align-center'>
-
-                <div className='d-flex flex-column'>
-                  <span>Цена:</span>
-                  <b>12 999 руб.</b>
-                </div>
-
-                <button className='addButton'>
-                  <img src="/plus.png" height={11} width={11} />
+                  <img src="/icons/AddButton.svg" height={25} width={25} />
                 </button>
               </div>
             </div>
